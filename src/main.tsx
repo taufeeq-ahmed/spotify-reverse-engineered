5ccada718cab5a30d7ad3acf98a6eecaf61d5d7b
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-09-01 16:40:10 +0530

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
+
+import './index.css';
 
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,11 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import App from '../App';
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className="p-2 flex gap-2">
-                <Link to="/">Home</Link> <Link to="/about">About</Link>
-            </div>
-            <hr />
+            <App />
             <Outlet />
             <TanStackRouterDevtools />
         </>
