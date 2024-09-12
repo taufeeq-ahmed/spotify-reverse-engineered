085a5f8dc42ca9cb0370264eaaa58c0232986f21
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-09-12 10:55:15 +0530

    custom hook for audio player added

--- src/features/audio-player-slice.ts
+++ src/features/audio-player-slice.ts
@@ -1,6 +1,22 @@
+import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '@/store/index';
 
-const audioPlayerInitState = {
+const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+interface Track {
+    title: string;
+    thumbnail: string;
+    src: string;
+}
+
+interface AudioPlayerState {
+    trackList: Track[];
+    currentTrack: Track | null;
+    currentIdx: number;
+}
+
+const audioPlayerInitState: AudioPlayerState = {
     trackList: [],
     currentTrack: null,
     currentIdx: 0,
@@ -24,3 +40,36 @@ export const { gotoNextTrack, gotoPreviousTrack } = audioPlayerSlice.actions;
 const audioPLayerReducer = audioPlayerSlice.reducer;
 
 export default audioPLayerReducer;
+
+// custom hook to interact with audioPlayerState
+export const useAudioPlayer = () => {
+    const dispatch = useDispatch();
+
+    const trackList = useTypedSelector(state => state.audioPlayer.trackList);
+
+    const currentTrack = useTypedSelector(
+        state => state.audioPlayer.currentTrack
+    );
+
+    const currentIdx = useTypedSelector(state => state.audioPlayer.currentIdx);
+
+    const playNextTrack = () => {
+        if (currentIdx < trackList.length - 1) {
+            dispatch(gotoNextTrack());
+        }
+    };
+
+    const playPreviousTrack = () => {
+        if (currentIdx > 0) {
+            dispatch(gotoPreviousTrack());
+        }
+    };
+
+    return {
+        trackList,
+        currentTrack,
+        currentIdx,
+        playNextTrack,
+        playPreviousTrack,
+    };
+};
