f3ec1032d5f38f65c9fc8f3cf8fff3dd7bc383d8
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-09-21 07:37:44 +0530

    shuffle button completed

--- src/components/app-components/audio-player/control-buttons/ShuffleButton.tsx
+++ src/components/app-components/audio-player/control-buttons/ShuffleButton.tsx
@@ -1,8 +1,27 @@
 import ShuffleIcon from '@/assets/icons/ShuffleIcon';
 import IconButton from '../../icon-button';
+import { useAudioPlayer } from '@/features/audio-player-slice';
 
 function ShuffleButton() {
-    return <IconButton icon={ShuffleIcon} tooltipContent="Enable Shuffle" />;
+    const { isShuffling, turnOnShuffle, turnOffShuffle } = useAudioPlayer();
+
+    return isShuffling ? (
+        <div className="flex flex-col justify-center items-center relative">
+            <IconButton
+                icon={ShuffleIcon}
+                tooltipContent="Disable Shuffle"
+                onClick={turnOffShuffle}
+                iconClassName="text-primary opacity-100"
+            />
+            <div className="w-[4px] h-[4px] bg-primary rounded-full relative top-[-4px]"></div>
+        </div>
+    ) : (
+        <IconButton
+            icon={ShuffleIcon}
+            tooltipContent="Enable Shuffle"
+            onClick={turnOnShuffle}
+        />
+    );
 }
 
 export default ShuffleButton;
